14d0d5db94159063b1e88ae4e65ca0f2d56f3919
Dennis <dennisalvey423@gmail.com>
2017-08-29 17:29:22 -0700

    what i have so far

---
+++ ClozeCard.js
@@ -0,0 +1,17 @@
+var clozeCard = function(fullText, cloze) {
+    this.fullText = fullText;
+    this.cloze = cloze;
+    }
+
+var partial = function() {
+    if (Process.argv[2] === cloze ) {
+    str.replace("George Washington", "_______");
+    }
+}
+
+
+
+
+
+
+module.exports = clozeCard;
